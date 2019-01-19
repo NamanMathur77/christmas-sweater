319c38197e950de647c537d761286070a0bdf5c2
NamanMathur <36097162+NamanMathur77@users.noreply.github.com>
2019-01-19 11:22:38 +0530

    changes in style

--- script.js
+++ script.js
@@ -2,20 +2,37 @@ var resetbtn=document.getElementById("reset");
 var home=document.getElementById("home");
 var selectmode1=document.getElementById("selectmode1");
 var hambtn=document.getElementById("ham");
+var facebookbtn=document.getElementById("facebook");
+var twitterbtn=document.getElementById("twitter");
+facebookbtn.addEventListener("mouseover",function(){
+  facebookbtn.style.borderRadius="10px"
+  facebookbtn.style.width="100px";
+  twitterbtn.style.right="100px"
+});
+facebookbtn.addEventListener("mouseout",function(){
+  facebookbtn.style.borderRadius="50%"
+  facebookbtn.style.width="40px";
+  twitterbtn.style.right="50px";
+});
 resetbtn.addEventListener("click",function(){
-  $("h1").remove();
-  $("p").remove();
+  $("h1").fadeOut("slow", function(){
+    $("h1").remove();
+  });
+  $("p").fadeOut("slow", function(){
+    $("p").remove();
+  });
+
   home.style.display="block";
   resetbtn.style.display="none"
 
 });
 
 function on() {
-  document.getElementById("overlay").style.display = "block";
+  $("#overlay").fadeIn("slow");
 }
 
 function off() {
-  document.getElementById("overlay").style.display = "none";
+  $("#overlay").fadeOut("slow");
   hambtn.classList.toggle("change");
 }
 function myFunction(x) {
